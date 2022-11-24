82fbe4145e39a03f4aab5444e3221a23cc98c269
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-11-24 12:37:40 +0100

    created the dump file

--- database.sql
+++ database.sql
@@ -1,63 +1,33 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+CREATE DATABASE cinq_majeur;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+use cinq_majeur;
 
+CREATE TABLE user (
+                      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      lastname VARCHAR(255) NOT NULL,
+                      firstname VARCHAR(255) NOT NULL,
+                      email VARCHAR(150) NOT NULL,
+                      `password` VARCHAR(255) NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE stepmom (
+                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                         userID INT NOT NULL,
+                         lastname VARCHAR(255) NOT NULL,
+                         firstname VARCHAR(255) NOT NULL,
+                         age INT NOT NULL
+);
 
---
--- Base de données :  `simple-mvc`
---
+CREATE TABLE trip (
+                      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      category INT NOT NULL,
+                      trip_name VARCHAR(255) NOT NULL,
+                      continent VARCHAR(50) NOT NULL,
+                      type VARCHAR(255),
+                      duration VARCHAR(255),
+                      network_coverage BOOL,
+                      distance INT
+);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE stepmom ADD CONSTRAINT fk_user_stepmom FOREIGN KEY(userID)
+    REFERENCES User (id);
