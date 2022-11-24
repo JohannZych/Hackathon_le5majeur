7c5fc641aa32ec29513090bbbc1612c8a12d2552
yazidham <79407250+yazidham@users.noreply.github.com>
2022-11-24 15:52:47 +0100

    adding the login and logout feature

---
+++ src/Controller/UserConnectionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserConnectionManager;
+
+class UserConnectionController extends AbstractController
+{
+    public function login(): string
+    {
+        $errors = [];
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+            $logs = array_map('trim', $_POST);
+            $logs = array_map('htmlentities', $logs);
+            // Validate data
+            $logsCheckValidation = new UserConnectionManager();
+            $errors = $logsCheckValidation->validation($logs);
+
+            // Check the connection
+            if (empty($errors)) {
+                $accessEmail = $logs['email'];
+                $accessPassword = $logs['password'];
+                $logsCheck = new UserConnectionManager();
+                $authorizationResult = $logsCheck->access($accessEmail, $accessPassword);
+
+                if ($authorizationResult === true) {
+                    //setting session
+                    $gettingId = new UserConnectionManager();
+                    $user = $gettingId->selectOneByEmail($accessEmail);
+                    $_SESSION['user_id'] = $user['id'];
+                    header('Location:/');
+                } else {
+                    $errors[] = "Les informations que vous avez saisi ne sont pas associées à un compte";
+                }
+            }
+        }
+        // Generate the web page
+        return $this->twig->render('Home/connection.html.twig', [
+            'errors' => $errors
+        ]);
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        unset($_SESSION['user_id']);
+        header('Location:/');
+    }
+}
